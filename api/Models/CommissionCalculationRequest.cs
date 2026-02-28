becd058132ba7c57bbb719bf281352e8d7e6f70c
Hrishikesh Jagtap <72089551+hrishi284@users.noreply.github.com>
2026-02-28 16:05:18 +0530

    Api : add commission request and response DTO models

---
+++ api/Models/CommissionCalculationRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace AvalphaTechnologies.CommissionCalculator.Models
+{
+    public class CommissionCalculationRequest
+    {
+        public int LocalSalesCount { get; set; }
+        public int ForeignSalesCount { get; set; }
+        public decimal AverageSaleAmount { get; set; }
+    }
+}
---
+++ api/Models/CommissionCalculationResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace AvalphaTechnologies.CommissionCalculator.Models
+{
+    public class CommissionCalculationResponse
+    {
+        public decimal AvalphaTechnologiesCommissionAmount { get; set; }
+        public decimal CompetitorCommissionAmount { get; set; }
+    }
+}
